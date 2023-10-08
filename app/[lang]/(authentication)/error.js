fa4e8d45cf4e91875ecba8b5c082aaf444a16ad9
Vj3ko <dizdar.vjeko@gmail.com>
2023-10-08 18:08:54 +0200

    added error boundaries

--- app/[lang]/(authentication)/error.js
+++ app/[lang]/(authentication)/error.js
@@ -6,12 +6,12 @@ export default function Error({ error, reset }) {
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error)
-    console.log('MAIN error.js file, error')
+    console.log('Authentication error')
   }, [error])
 
   return (
     <div>
-      <h2>Something went wrong in MAIN LAYOUT component!</h2>
+      <h2>Something went wrong in authentication component!</h2>
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
